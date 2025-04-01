b05f460f73f46fa0e3095022029776a8b0b544b3
gracegerwe <86850911+gracegerwe@users.noreply.github.com>
2025-03-31 22:51:10 -0700

    printing a point

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <gp_Pnt.hxx>
+#include <iostream>
+
+int main() {
+    gp_Pnt p(1.0, 2.0, 3.0);
+    std::cout << "3D Point: ("
+              << p.X() << ", "
+              << p.Y() << ", "
+              << p.Z() << ")\n";
+    return 0;
+}
