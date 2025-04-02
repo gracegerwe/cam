dd204ef5b48d6ff63e1dbfef8f434d7d2c2580ac
gracegerwe <86850911+gracegerwe@users.noreply.github.com>
2025-04-01 21:39:13 -0700

    compiled code + env setup!

--- main.cpp
+++ main.cpp
@@ -2,10 +2,8 @@
 #include <iostream>
 
 int main() {
-    gp_Pnt p(1.0, 2.0, 3.0);
-    std::cout << "3D Point: ("
-              << p.X() << ", "
-              << p.Y() << ", "
-              << p.Z() << ")\n";
-    return 0;
+  gp_Pnt p(1.0, 2.0, 3.0);
+  std::cout << "3D Point: (" << p.X() << ", " << p.Y() << ", " << p.Z()
+            << ")\n";
+  return 0;
 }
